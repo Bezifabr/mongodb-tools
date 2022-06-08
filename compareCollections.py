5e7bfd266634a91e82935af782afafbfcdfe8261
M.Saranda <maciej@saranda.dev>
2022-06-08 17:38:14 +0200

    Update compareCollections.py

--- compareCollections.py
+++ compareCollections.py
@@ -1,7 +1,11 @@
+from itertools import islice
 from pprint import pprint
 
 from collections import Counter
 from pymongo import MongoClient
+from datetime import datetime
+
+filename = "logs/" + datetime.today().strftime('%Y-%m-%d_%H%M%S') + "_result.log"
 
 
 def get_database_connection(nodes, username, password, db_name, auth_source='admin', auth_mechanism='SCRAM-SHA-1'):
@@ -16,10 +20,13 @@ def get_database_connection(nodes, username, password, db_name, auth_source='adm
 
 
 def _print_bigger_collection_name(collection_name_1, collection_name_2, collection_size_1, collection_size_2):
-    if collection_size_1 > collection_size_2:
-        pprint(collection_name_1 + " is bigger than " + collection_name_2)
-    if collection_size_2 > collection_size_1:
-        pprint(collection_name_2 + " is bigger than " + collection_name_1)
+    with open(filename, "a") as log_file:
+        if collection_size_1 > collection_size_2:
+            log_file.write(collection_name_1 + " is bigger than " + collection_name_2 + "\n")
+            print(collection_name_1 + " is bigger than " + collection_name_2)
+        if collection_size_2 > collection_size_1:
+            log_file.write(collection_name_2 + " is bigger than " + collection_name_1 + "\n")
+            print(collection_name_2 + " is bigger than " + collection_name_1)
 
 
 def _compare_two_collections_by_number_of_documents(collection_1, collection_2):
@@ -27,15 +34,17 @@ def _compare_two_collections_by_number_of_documents(collection_1, collection_2):
     collection_size_2 = collection_2.count_documents({})
     collection_name_1: str = collection_1.name + " from " + collection_1.database.name
     collection_name_2: str = collection_2.name + " from " + collection_2.database.name
-
-    if collection_size_2 == collection_size_1:
-        pprint("There are " + str(collection_size_1) + " documents in both collections!")
-        return True
-    # Print number of documents
-    pprint(collection_name_1 + " has " + str(collection_size_1) + " elements")
-    pprint(collection_name_2 + " has " + str(collection_size_2) + " elements")
-    _print_bigger_collection_name(collection_name_1, collection_name_2, collection_size_1, collection_size_2)
-    return False
+    with open(filename, "a") as log_file:
+        if collection_size_2 == collection_size_1:
+            log_file.write("There are " + str(collection_size_1) + " documents in both collections!\n")
+            return True
+        # Print number of documents
+        log_file.write(collection_name_1 + " has " + str(collection_size_1) + " elements\n")
+        log_file.write(collection_name_2 + " has " + str(collection_size_2) + " elements\n")
+        print(collection_name_1 + " has " + str(collection_size_1) + " elements")
+        print(collection_name_2 + " has " + str(collection_size_2) + " elements")
+        _print_bigger_collection_name(collection_name_1, collection_name_2, collection_size_1, collection_size_2)
+        return False
 
 
 def _compare_two_collections_by_differing_documents(collection_1, collection_2):
@@ -57,36 +66,75 @@ def _compare_two_collections_by_differing_documents(collection_1, collection_2):
 
     if len(differences) == 0:
         return True
-    pprint(differences)
+    with open(filename, "a") as log_file:
+        pprint(differences, log_file)
+        log_file.write("\nNumber of differences: " + str(len(differences)) + "/n")
+    if len(differences) >= 10:
+        pprint(list(islice(differences.items(), 10)))
+        print("...")
+        print("Check the log file for more details: " + str(filename))
+    else:
+        pprint(differences)
     print("Number of differences: " + str(len(differences)))
     return False
 
 
 def run_tests(_collection_names, _db_1, _db_2):
     _general_result = True
+    _error_count = 0
+    _success_count = 0
+    _differing_collections = []
+
     for collection_name_1, collection_name_2 in _collection_names:
-        print()
-        print("--------------------")
-        pprint("Compare " + collection_name_1 + " with " + collection_name_2)
+        with open(filename, "a") as log_file:
+            log_file.write('--------------------\n')
+            log_file.write("Compare " + collection_name_1 + " with " + collection_name_2 + "\n")
         test1 = _compare_two_collections_by_differing_documents(_db_1[collection_name_1], _db_2[collection_name_2])
         test2 = _compare_two_collections_by_number_of_documents(_db_1[collection_name_1], _db_2[collection_name_2])
-        if test1 and test2:
-            print("[SUCCESS] There are no differences!")
-        else:
-            print("[FAILED] There are some differences!")
-            _general_result = False
 
-    print()
-    print()
-    print("________________")
+        with open(filename, "a") as log_file:
+            if test1 and test2:
+                log_file.write("[SUCCESS] There are no differences!\n")
+                _success_count += 1
+            else:
+                log_file.write("[FAILED] There are some differences!\n")
+                print("------------------------")
+                print("[FAILED] There are some differences!")
+                _error_count += 1
+                _differing_collections.append((collection_name_1, collection_name_2))
+                _general_result = False
+
+    with open(filename, "a") as log_file:
+        log_file.write("\n")
+        log_file.write("----------------\n")
+
     if _general_result:
-        pprint("[OVERALL RESULT]: SUCCESS!")
+        with open(filename, "a") as log_file:
+            log_file.write("[OVERALL RESULT]: SUCCESS!\n")
+        with open(filename, "r") as log_file:
+            print(log_file.read())
     else:
-        pprint("[OVERALL RESULT]: FAILED!")
+        with open(filename, "a") as log_file:
+            log_file.write("[OVERALL RESULT]: FAILED!\n")
+        print("[OVERALL RESULT]: FAILED!\n")
+
+    with open(filename, "a") as log_file:
+        log_file.write("Number of tests: " + str(len(_collection_names)) + "\n")
+        log_file.write("Number of failed tests: " + str(_error_count) + "\n")
+        log_file.write("Number of passed tests: " + str(_success_count) + "\n")
+        log_file.write("-----------------------\n")
+        log_file.write("Differing collections: \n")
+        for col1, col2 in _differing_collections:
+            log_file.write("-" + col1 + " =/= " + col2 + "\n")
+
     print("Number of tests: " + str(len(_collection_names)))
-    print("----------------")
-    print()
-    print()
+    print("Number of failed tests: " + str(_error_count))
+    print("Number of passed tests: " + str(_success_count))
+    print("-----------------------")
+    print("Differing collections: ")
+    for col1, col2 in _differing_collections:
+        print("-" + col1 + " =/= " + col2)
+
 
 #     
 # USAGE EXAMPLE
